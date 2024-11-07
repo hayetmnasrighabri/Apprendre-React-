6396a4bd2ac618df7608ea311d54e2efd2899b1a
hayetmnasri <hayetmnasri@hotmail.fr>
2024-11-07 09:08:53 +0100

    le hook useState (count)

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
-import React, { Children } from 'react'
+import React, { useState } from 'react'
 
-const todos=[
-  'Présenter react' ,
-  'Présenter le JSX' ,
-  'Créer des composants'
-]
 function App() {
- 
-  return ( 
-    <div>
-     <Title color="green" id="monid" className="demo" data-demo="demo">Mon composant</Title>       
-    <input type='text'/>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus ipsa eos deserunt, eligendi mollitia fugiat ullam sequi, facere ut perspiciatis voluptatem laborum omnis aut quae. Inventore dolorum corrupti harum eius.
-    </p>
-    <ul>
-      {todos.map(todo=>
-        <li key={todo}>{todo}</li>
-      )}
-    </ul>
-    </div>
-  )
-}
-function Title({color, children, ...props}){
-  return(
-    <h1 style={{color: color}} {...props}>{children}</h1>
+  const [count, setCount]=useState(0)
+  const incrementer=()=>{
+  setCount(count+1)
+ }
+  return (
+    <>
+    <p>Compteur: {count}</p>
+    <button onClick={incrementer}>Incrementer</button>
+    </> 
   )
 }
 
